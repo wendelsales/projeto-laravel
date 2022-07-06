4d8643c6fe39affbb070113c5f528fcd40f5e56c
wendelsales <uendel.1@hotmail.com>
2022-07-06 00:46:08 -0300

    novo comite

---
+++ routes/web.php
@@ -0,0 +1,16 @@
+<?php
+
+use App\Http\Controllers\SiteController;
+use Illuminate\Support\Facades\Route;
+
+//Route::get('/', function () {
+//    return 'ainda tou na rota';
+//    //return view('bemvindo');
+//});
+
+
+
+//SiteController -----rota
+//tipo da rota  --  controller  --  function do controller
+Route::get('/',[SiteController::class, 'index']);
+Route::get('/sair',[SiteController::class, 'sair']);
